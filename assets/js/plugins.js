4150decae8631009e7dce5d44adcd9852b73d76f
Michał T <michterlecki@gmail.com>
2019-09-01 13:30:14 +0200

    Initial commit

---
+++ assets/js/plugins.js
@@ -0,0 +1,66 @@
+// jQuery JavaScript Library v3.4.1
+import jQuery from "jquery";
+window.$ = window.jQuery = jQuery;
+
+import rwdMedia from './rwdMedia.js';
+window.rwdMedia = rwdMedia;
+
+// waves
+import {wavesInit} from './waves.js';
+window.wavesInit = wavesInit;
+
+// vendor: moment v2.24.0,
+import moment from 'moment';
+window.moment = moment;
+
+// vendor: daterangepicker v3.0.5,
+import daterangepicker from 'daterangepicker';
+window.daterangepicker = daterangepicker;
+
+
+function tagglePass(elem) {
+    const $this = $(elem);
+    const thisType = $this.attr('type');
+
+    if (thisType === 'password') {
+        $this.attr('type', 'text');
+        $this.next()
+        $('.icon-eye-slash', $this.next()).removeClass('d-none');
+        $('.icon-eye', $this.next()).addClass('d-none');
+    }
+    else if (thisType === 'text') {
+        $this.attr('type', 'password');
+        $('.icon-eye-slash', $this.next()).addClass('d-none');
+        $('.icon-eye', $this.next()).removeClass('d-none');
+    }
+}
+
+
+window.tagglePass = tagglePass;
+
+$(document).ready( function() {
+
+    $('.date-picker').daterangepicker({
+        timePicker: true,
+        timePicker24Hour: true,
+        timePickerIncrement: 1,
+        cancelClass: "btn-secondary",
+        timePickerSeconds: false,
+        locale: {
+            format: 'HH:mm:ss'
+        }
+    }).on('show.daterangepicker', function (ev, picker) {
+        picker.container.find(".calendar-table").hide();
+    });
+
+    $('.single-date-picker').daterangepicker({
+        singleDatePicker: true,
+        showDropdowns: true,
+        startDate: '+1d',
+        cancelClass: "btn-secondary",
+        locale: {
+            format: 'DD/MM/YYYY'
+        }
+    });
+
+});
---
+++ assets/js/waves.js
@@ -0,0 +1,8 @@
+// vendor: node-waves v0.7.6,
+import Waves from 'node-waves';
+
+export function wavesInit() {
+    Waves.attach('.btn-primary', ['waves-button', 'waves-classic', 'waves-light']);
+    Waves.attach('.btn-outline-primary', ['waves-button', 'waves-classic', 'waves-light']);
+    Waves.init();
+}
